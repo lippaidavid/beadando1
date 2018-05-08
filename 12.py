aefe3335332a6d54e029ff080dffa7e831de1e45
lippaidavid <37172657+lippaidavid@users.noreply.github.com>
2018-05-08 19:19:32 +0200

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,9 +1,10 @@
+import math
 def isPrime(n):
     if n == 2:
         return True
     if n % 2 == 0 or n == 1:
         return False
-    for i in range(3, (int(n ** 0.5) + 1), 2):
+    for i in range(3, (int(math.sqrt(n)) + 1), 2): #(int(n ** 0.5))
         if n % i == 0:
             return False
     return True
